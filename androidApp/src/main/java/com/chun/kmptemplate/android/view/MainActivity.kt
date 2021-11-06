46dc20973f2c87259460c06ba20d449c1b68930c
chungnh <nchung.dev@gmail.com>
2021-11-06 10:03:05 +0700

    Move MainActivity.kt to view package

--- androidApp/src/main/java/com/chun/kmptemplate/android/view/MainActivity.kt
+++ androidApp/src/main/java/com/chun/kmptemplate/android/view/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.chun.kmptemplate.android
+package com.chun.kmptemplate.android.view
 
 import androidx.appcompat.app.AppCompatActivity
+import com.chun.kmptemplate.android.R
 
 class MainActivity : AppCompatActivity(R.layout.main)
